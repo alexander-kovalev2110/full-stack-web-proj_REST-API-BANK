454e433a184436ab361a4da97f2e7eab5606e808
alexander-kovalev2110 <alexander.kovalev@yahoo.com>
2023-05-02 17:51:18 +0300

    Controller/Service update

--- back-end/php-symfony-mysql/src/Controller/CustomerController.php
+++ back-end/php-symfony-mysql/src/Controller/CustomerController.php
@@ -2,12 +2,12 @@
 
 namespace App\Controller;
 
+//use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Customer;
-use Doctrine\ORM\EntityManagerInterface;
 
 class CustomerController extends AbstractController
 {
@@ -18,60 +18,25 @@ class CustomerController extends AbstractController
         $this->entityManager = $entityManager;
     }
 
-    // Adding of a customer
-    #[Route('/customer/{name}/{pw}', name: 'add_customer', methods: ['POST'])]
-    public function addCustomer(string $name, string $pw): Response
+    // Customer create
+    #[Route('/customer/{name}/{pw}', name: 'create_customer', methods: ['POST'])]
+    public function createCustomer(string $name, string $pw): Response
     {
-        // Checking the customer for existence
-        $customer = $this->entityManager->getRepository(Customer::class)
-        ->findOneBy(['name' => $name, 'pw' => $pw]);
-        if ($customer) {
-            return new JsonResponse([
-                'message' => 'Customer already exists.' 
-            ], 400); 
-        }
+        $service = new CustomerService($this->entityManager);
+        $result = $service->createServ($name, $pw);
 
-        $customer = new Customer();
-        $customer->setName($name);
-        $customer->setPw($pw);
-
-        // Saving $customer to Customer DB
-        $this->entityManager->persist($customer);
-        $this->entityManager->flush();
-
-        // Getting customrId from Customer DB for testing and response
-        $customer = $this->entityManager->getRepository(Customer::class)
-                ->findOneBy(['name' => $name, 'pw' => $pw]);
-        
-        if ($customer) {
-            return new JsonResponse([
-                'customerId' => $customer->getId(),
-            ], 200);
-        }
-        else {
-            return new JsonResponse([
-                'message' => 'Customer is not available.' 
-            ], 400); 
-        }
+        $cod = ($result['customerId'] ?? null) ? 200 : 400;
+        return new JsonResponse($result, $cod);
     }
 
     // Customer login
     #[Route('/customer/{name}/{pw}', name: 'login_customer', methods: ['GET'])]
     public function loginCustomer(string $name, string $pw): Response
     {
-        // Getting customrId from Customer DB
-        $customer = $this->entityManager->getRepository(Customer::class)
-                ->findOneBy(['name' => $name, 'pw' => $pw]);
-        
-        if ($customer) {
-            return new JsonResponse([
-                'customerId' => $customer->getId(),
-            ], 200);
-        }
-        else {
-            return new JsonResponse([
-                'message' => 'Customer is not available.'
-            ], 400); 
-        }
+        $service = new CustomerService($this->entityManager);
+        $result = $service->loginServ($name, $pw);
+
+        $cod = ($result['customerId'] ?? null) ? 200 : 400;
+        return new JsonResponse($result, $cod);
     }
 }
